d3f10cf3afa038b3201a098701b3102c26dabc62
Kristian Freeman <kristian@kristianfreeman.com>
2014-02-18 14:36:06 -0800

    Add overlay, update to 0.1.0

--- README.md
+++ README.md
@@ -14,3 +14,14 @@ Add file to your Ember project manually
 ## Usage
 
 `{{ gumroad "<your_product_id"> }}`
+
+Additional options:
+
+Pass in `type=overlay` for the [overlay type](https://gumroad.com/overlay)
+instead of the standard embed. The options on that page can be used here
+- `text=''` can be used to pass in the text for the overlay link (defaults to
+  "Buy my product", `wanted=true` for the wanted config (clicking the "Buy" link
+  automatically opens to the payment section of the overlay, instead of the product
+  description), and `locale=true` to auto-set the locale.
+
+`{{ gumroad "demo" overlay=true text="PLZ BUY THNX" wanted=true }}`
--- ember-gumroad-helper.js
+++ ember-gumroad-helper.js
@@ -1,9 +1,26 @@
 (function() {
-  Ember.Handlebars.helper('gumroad', function(gumroadID) {
-    var escaped;
+  Ember.Handlebars.helper('gumroad', function(gumroadID, opts) {
     Ember.assert("You must provide the Gumroad product ID", !!gumroadID.length);
-    escaped = Handlebars.Utils.escapeExpression(gumroadID);
-    return new Handlebars.SafeString('<script type="text/javascript" src="https://gumroad.com/js/gumroad-embed.js"></script>\n<div class="gumroad-product-embed" data-gumroad-product-id="' + escaped + '"></div>');
+    var options = opts.hash;
+    var escaped = Handlebars.Utils.escapeExpression(gumroadID);
+    if (options.type === "overlay") {
+      var text = options.text || "Buy my product";
+      if (options.wanted) { var wanted = true; }
+      if (options.locale) { var locale = true; }
+
+      if (wanted || locale) {
+        var params = "?";
+        if (wanted) { params += "wanted=true"; }
+        if (wanted && locale) { params += "&"; }
+        if (locale) { params += "locale=true"; }
+      } else {
+        var params = "";
+      }
+
+      return new Handlebars.SafeString('<script type="text/javascript" src="https://gumroad.com/js/gumroad.js"></script>\n<a href="https://gum.co/' + escaped + params + '" class="gumroad-button">' + text + '</a>')
+    } else {
+      return new Handlebars.SafeString('<script type="text/javascript" src="https://gumroad.com/js/gumroad-embed.js"></script>\n<div class="gumroad-product-embed" data-gumroad-product-id="' + escaped + '"></div>');
+    }
   });
 
 }).call(this);
